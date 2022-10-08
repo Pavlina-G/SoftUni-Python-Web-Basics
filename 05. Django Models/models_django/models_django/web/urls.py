c85220bf4fbdff122ea7fc95f7408bee849d4c70
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-10-09 00:10:29 +0300

    Update urls.py

--- 05. Django Models/models_django/models_django/web/urls.py
+++ 05. Django Models/models_django/models_django/web/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = ()
+from django.urls import path
+
+from models_django.web.views import index, delete_employee, department_details
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('delete/<int:pk>/', delete_employee, name='delete employee'),
+    # path('department/<int:pk>/', department_details, name='details department'),
+    path('department/<int:pk>/<slug:slug>/', department_details, name='details department'),
+    # http://127.0.0.1:8000/department/2/engineering/
+)
