49e89339463f932e809cc6054a98a6594f63720f
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2023-01-15 18:15:33 +0200

    Delete 05. Django Models/models_django directory

--- 05. Django Models/models_django/models_django/web/validators.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-
-def validate_before_today(value):
-    # if invalid - raise ValidationError
-    if date.today() < value:
-        raise ValueError(f'{value} is not valid')
