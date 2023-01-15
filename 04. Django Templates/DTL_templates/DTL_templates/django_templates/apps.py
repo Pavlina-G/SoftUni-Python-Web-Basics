e9e324b34445937e2831b4be25a208b60d78ddb7
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2023-01-15 18:15:20 +0200

    Delete 04. Django Templates directory

--- 04. Django Templates/DTL_templates/DTL_templates/django_templates/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoTemplatesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DTL_templates.django_templates'
--- 04. Django Templates/DTL_templates/DTL_templates/django_templates/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from DTL_templates.django_templates.views import index, redirect_to_home, about
-
-urlpatterns = (
-    path('', index, name='index'),
-    path('go-to-home/', redirect_to_home, name='redirect to home'),
-    path('about/', about, name='about'),
-)
